fb887800bdacb59b54d332877cf9e541e5742bc0
Pablo Torres <179417434+GrizzlyTorres@users.noreply.github.com>
2026-04-29 10:38:24 -0300

    Add files via upload

---
+++ Exercicios_Java_Heranca/exercício1/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Moto minhaMoto = new Moto("yamaha", "MT-07", 690);
+        minhaMoto.exibirDados();
+    }
+}
---
+++ Exercicios_Java_Heranca/exercício1/src/Moto.java
@@ -0,0 +1,16 @@
+public class Moto extends Veiculo{
+
+    private int cilindradas;
+
+    public Moto(String marca, String modelo, int cilindradas) {
+        super(marca, modelo);
+        this.cilindradas = cilindradas;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("cilindradas: " + cilindradas + "cc");
+    }
+    
+}
