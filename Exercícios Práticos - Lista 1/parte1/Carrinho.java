596f212d1132f67bc5c39e89e28f9d5d7c1d8102
Pablo Torres <179417434+GrizzlyTorres@users.noreply.github.com>
2026-05-27 10:17:28 -0300

    Create Carrinho.java

---
+++ Exercícios Práticos - Lista 1/parte1/Carrinho.java
@@ -0,0 +1,25 @@
+class CarrinhoDeCompras {
+    private ArrayList<Produto> produtos;
+
+    public CarrinhoDeCompras() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adicionarProduto(Produto p) {
+        produtos.add(p);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Produto p : produtos) {
+            total += p.getPreco();
+        }
+        return total;
+    }
+
+    public void aplicarDescontoGeral(double percentual) {
+        for (Produto p : produtos) {
+            p.aplicarDesconto(percentual);
+        }
+    }
+}
